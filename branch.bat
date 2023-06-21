9c6a5d475db9c42898befab7870e678e7f26e2b5
Melvyn212 <dehundeabdala@gmail.com>
2023-06-21 23:30:52 +0200

    ASR initialisation

--- branch.bat
+++ branch.bat
@@ -7,10 +7,9 @@ mkdir assistant-vocal\nlg\models
 mkdir assistant-vocal\tts\models
 mkdir assistant-vocal\tests
 
-echo.> assistant-vocal\asr\asr.py
-echo.> assistant-vocal\nlu\nlu.py
-echo.> assistant-vocal\dialogue_management\dialogue.py
-echo.> assistant-vocal\nlg\nlg.py
-echo.> assistant-vocal\tts\tts.py
-echo.> assistant-vocal\main.py
-
+copy nul assistant-vocal\asr\asr.py > nul 2>&1
+copy nul assistant-vocal\nlu\nlu.py > nul 2>&1
+copy nul assistant-vocal\dialogue_management\dialogue.py > nul 2>&1
+copy nul assistant-vocal\nlg\nlg.py > nul 2>&1
+copy nul assistant-vocal\tts\tts.py > nul 2>&1
+copy nul assistant-vocal\main.py > nul 2>&1
