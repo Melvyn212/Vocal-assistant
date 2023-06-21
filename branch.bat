4052a9004da33c00f38d51947842d54af7e71722
Melvyn212 <dehundeabdala@gmail.com>
2023-06-21 22:27:51 +0200

    Set of the project architecture

---
+++ branch.bat
@@ -0,0 +1,16 @@
+@echo off
+
+mkdir assistant-vocal\asr\models
+mkdir assistant-vocal\nlu\models
+mkdir assistant-vocal\dialogue_management\models
+mkdir assistant-vocal\nlg\models
+mkdir assistant-vocal\tts\models
+mkdir assistant-vocal\tests
+
+echo.> assistant-vocal\asr\asr.py
+echo.> assistant-vocal\nlu\nlu.py
+echo.> assistant-vocal\dialogue_management\dialogue.py
+echo.> assistant-vocal\nlg\nlg.py
+echo.> assistant-vocal\tts\tts.py
+echo.> assistant-vocal\main.py
+
