960706c5de094dbd1dffafebd692b7b6fdda4893
Melvyn212 <dehundeabdala@gmail.com>
2023-06-23 08:36:11 +0200

    BROUILLONS

--- assistant-vocal/main.py
+++ assistant-vocal/main.py
@@ -1,26 +1,24 @@
-from asr.asr import ASR
+import ASR
 from nlu.nlu import NLU
 from dialogue_management.dialogue import DialogueManager
-from nlg.nlg import NLG
-from tts.tts import TTS
+from tts.tts import VoiceSynthesizer
 from wakeword.wake_word import WakeWordDetector
 from audio_preprocessing.audio_preprocessing import AudioPreprocessor
 import pyaudio
 
 class VoiceAssistant:
     def __init__(self, config):
-        self.asr = ASR(config['asr_model_path'])
-        self.nlu = NLU(config['nlu_model_path'])
-        self.dialogue_manager = DialogueManager(config['dialogue_model_path'])
-        self.nlg = NLG(config['nlg_model_path'])
-        self.tts = TTS(config['tts_model_path'])
+        self.nlu = NLU(model_name=config['nlu_model_name'])
+        self.dialogue_manager = DialogueManager(model_name=config['dialogue_model_name'])
+        self.nlg = NLG(model_name=config['nlg_model_name'])
+        self.VoiceSynthesizer = VoiceSynthesizer(config['tts_model_path'])
         self.wake_word_detector = WakeWordDetector(config['wake_words'])
         self.audio_preprocessor = AudioPreprocessor(config['audio_preprocessing_model_path'])
         self.audio = pyaudio.PyAudio()
         self.listening = True
 
     def run(self):
-        self.tts.speak("Hi, what can I do for you?")
+        self.VoiceSynthesizer.speak("Hi, what can I do for you?")
         while self.listening:
             wake_word = self.wake_word_detector.listen()
             if wake_word and self.wake_word_detector.is_active():
@@ -34,7 +32,7 @@ class VoiceAssistant:
                 intent, entities = self.nlu.process(text)
                 response = self.dialogue_manager.respond(intent, entities)
                 generated_text = self.nlg.generate(response)
-                self.tts.speak(generated_text)
+                self.VoiceSynthesizer.speak(generated_text)
 
     def listen_for_audio(self):
         stream = self.audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
@@ -58,13 +56,12 @@ class VoiceAssistant:
 
 if __name__ == '__main__':
     config = {
-        'asr_model_path': 'asr/models/',
-        'nlu_model_path': 'nlu/models/',
-        'dialogue_model_path': 'dialogue_management/models/',
-        'nlg_model_path': 'nlg/models/',
-        'tts_model_path': 'tts/models/',
+        'nlu_model_name': 'bert-base-uncased',
+        'dialogue_model_name': 'bert-base-uncased',
+        'nlg_model_name': 'gpt2',
+        'tts_model_path': 'tts/',
         'wake_words': ['hey assistant', 'ok assistant'],
-        'audio_preprocessing_model_path': 'audio_preprocessing/models/',
+        'audio_preprocessing_model_path': 'audio_preprocessing/',
     }
     assistant = VoiceAssistant(config)
     assistant.run()
--- assistant-vocal/nlu/nlu.py
+++ assistant-vocal/nlu/nlu.py
@@ -1,12 +1,20 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
 
 class NLU:
     def __init__(self, model_name):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        self.model_name = model_name
+        self.tokenizer = BertTokenizer.from_pretrained(self.model_name)
+        self.model = BertForSequenceClassification.from_pretrained(self.model_name)
 
     def predict_intent(self, text):
-        inputs = self.tokenizer.encode(text, return_tensors='pt')
-        outputs = self.model(inputs)
-        predicted_id = outputs.logits.argmax(-1).item()
-        return self.model.config.id2label[predicted_id]
+        inputs = self.tokenizer.encode_plus(text, return_tensors='pt')
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+
+        outputs = self.model(input_ids, attention_mask=attention_mask)
+        predicted_id = torch.argmax(outputs.logits, dim=1).item()
+        predicted_label = self.model.config.id2label[predicted_id]
+
+        return predicted_label
+nlu = NLU(model_name="bert-base-uncased")
