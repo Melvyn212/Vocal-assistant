d61b6d998f867975c91f5f5dbe32ecc7439ac173
Melvyn212 <dehundeabdala@gmail.com>
2023-06-23 18:12:00 +0200

    update of sound save

--- assistant-vocal/audio_preprocessing/audio_preprocessing.py
+++ assistant-vocal/audio_preprocessing/audio_preprocessing.py
@@ -4,10 +4,26 @@ import torchaudio
 
 class AudioPreprocessor:
     def __init__(self, sample_rate, threshold):
+        """
+        Constructeur de la classe AudioPreprocessor.
+        
+        Args:
+            sample_rate (int): le taux d'échantillonnage de l'audio à prétraiter.
+            threshold (float): le seuil utilisé pour détecter les parties utiles de l'audio.
+        """
         self.sample_rate = sample_rate
         self.threshold = threshold
 
     def envelope(self, signal):
+        """
+        Crée un masque pour le signal audio en fonction du seuil.
+
+        Args:
+            signal (np.array): le signal audio à masquer.
+
+        Returns:
+            np.array: un masque indiquant les parties du signal à conserver.
+        """
         mask = []
         signal = pd.Series(signal).apply(np.abs) 
         signal_mean = signal.rolling(window = int(self.sample_rate/10), min_periods = 1, center = True).mean()
@@ -19,10 +35,28 @@ class AudioPreprocessor:
         return np.array(mask)
 
     def load_audio(self, audio_file):
+        """
+        Charge un fichier audio et le convertit en une forme d'onde.
+
+        Args:
+            audio_file (str): le chemin vers le fichier audio à charger.
+
+        Returns:
+            np.array: la forme d'onde du fichier audio.
+        """
         waveform, _ = torchaudio.load(audio_file)
         return waveform.numpy()
 
     def preprocess_audio(self, audio_file):
+        """
+        Charge un fichier audio, le convertit en une forme d'onde, et applique le masque créé par la fonction enveloppe.
+
+        Args:
+            audio_file (str): le chemin vers le fichier audio à prétraiter.
+
+        Returns:
+            np.array: le signal audio prétraité.
+        """
         signal = self.load_audio(audio_file)
         mask = self.envelope(signal)
         return signal[mask]
--- assistant-vocal/wakeword/wake_word.py
+++ assistant-vocal/wakeword/wake_word.py
@@ -5,24 +5,55 @@ import pyaudio
 
 class WakeWordDetector:
     def __init__(self, wake_words=['jarvis']):
+        """
+        Initialise une instance de WakeWordDetector avec des mots de réveil spécifiques.
+
+        Args:
+            wake_words (list, optional): Une liste de mots de réveil. Par défaut à ['jarvis'].
+        """
+        # Créer une instance Porcupine avec les mots de réveil
         self.wake_words = wake_words
         self.porcupine = pvporcupine.create(keywords=wake_words)
+
+        # Ouvrir un flux audio avec PyAudio pour écouter les mots de réveil
         self.audio_stream = pyaudio.PyAudio().open(
             rate=self.porcupine.sample_rate,
             channels=1,
             format=pyaudio.paInt16,
             input=True,
             frames_per_buffer=self.porcupine.frame_length)
+        
+        # Garder une trace du dernier moment où un mot de réveil a été détecté
         self.last_activity = time.time()
 
     def listen(self):
+        """
+        Écoute en continu les mots de réveil et renvoie le mot de réveil détecté.
+        
+        Returns:
+            str: Le mot de réveil détecté.
+        """
         while True:
+            # Lire les données audio du flux
             pcm = self.audio_stream.read(self.porcupine.frame_length)
+            
+            # Décoder les données audio
             pcm = struct.unpack_from("h" * self.porcupine.frame_length, pcm)
+            
+            # Utiliser Porcupine pour traiter les données audio
             result = self.porcupine.process(pcm)
+
+            # Si un mot de réveil a été détecté, mettre à jour l'heure de la dernière activité
+            # et renvoyer le mot de réveil détecté
             if result >= 0:
                 self.last_activity = time.time()
                 return self.wake_words[result]
 
     def is_active(self):
+        """
+        Vérifie si un mot de réveil a été détecté dans les 30 dernières secondes.
+
+        Returns:
+            bool: True si un mot de réveil a été détecté dans les 30 dernières secondes, sinon False.
+        """
         return time.time() - self.last_activity < 30
